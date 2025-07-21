24a7472f162850fe48da5371ac250f0b645c9886
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-21 21:01:21 +0530

    changes

--- MegaBlog/src/components/PostCard.jsx
+++ MegaBlog/src/components/PostCard.jsx
@@ -3,6 +3,7 @@ import appwriteService from "../appwrite/config"
 import {Link} from 'react-router-dom'
 
 
+
 function PostCard({$id,title,featuredImage}) {
   return (
     <Link to={`/post/${$id}`}>
