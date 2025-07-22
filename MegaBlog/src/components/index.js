141851396b9327ff2d059f52fdd635ac7d9a897e
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-22 21:06:35 +0530

    changes

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -4,10 +4,25 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from './Login';
+
+import PostForm from './post-form/PostForm';
+import PostCard from "./PostCard"
+import AuthLayout from './AuthLayout'
+
 export{
     Header,
     Footer,
     Container,
     Logo,
     LogoutBtn,
+    Signup,
+    RTE,
+    Login,
+
+    PostForm,
+    PostCard,
+    AuthLayout,
+
 }
