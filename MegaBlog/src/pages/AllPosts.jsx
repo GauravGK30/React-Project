7e30a759cfa8be70b03e9b9a28b603c91b4fe246
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-23 22:23:07 +0530

    final stage

--- MegaBlog/src/pages/AllPosts.jsx
+++ MegaBlog/src/pages/AllPosts.jsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import appwriteService from "../appwrite/config"
 import { PostCard,Container } from '../components'
+
 function AllPosts() {
 
     const [posts,setPosts] = useState([])
@@ -16,7 +17,7 @@ function AllPosts() {
             <div className='flex flex-wrap'>
                 {posts.map((post)=>(
                     <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard post={post}/>
+                        <PostCard {...post}/>
                     </div>
                 ))}
             </div>
