d83498c5a6dee9bf391afb71f0507882a6c0ff35
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-09-01 21:29:15 +0530

    mega

--- MegaBlog/src/pages/AllPosts.jsx
+++ MegaBlog/src/pages/AllPosts.jsx
@@ -1,27 +1,29 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import appwriteService from "../appwrite/config"
-import { PostCard,Container } from '../components'
+import { PostCard, Container } from '../components'
 
 function AllPosts() {
+  const [posts, setPosts] = useState([])
 
-    const [posts,setPosts] = useState([])
-    useEffect(()=>{},[])
-    appwriteService.getPosts([]).then((posts)=>{
-        if(posts){
-            setPosts(posts.documents)
-        }
+  useEffect(() => {
+    appwriteService.getPosts([]).then((result) => {
+      if (result) {
+        setPosts(result.documents)
+      }
     })
+  }, [])
+
   return (
     <div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-                {posts.map((post)=>(
-                    <div key={post.$id} className='p-2 w-1/4'>
-                        <PostCard {...post}/>
-                    </div>
-                ))}
+      <Container>
+        <div className='flex flex-wrap'>
+          {posts.map((post) => (
+            <div key={post.$id} className='p-2 w-1/4'>
+              <PostCard {...post} />
             </div>
-        </Container>
+          ))}
+        </div>
+      </Container>
     </div>
   )
 }
